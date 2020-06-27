47be95c9daf3c01c6a1e3f0709b4bb833fea5721
Tushar Saurabh <tusharacc@gmail.com>
2020-06-26 22:30:53 -0400

    LCD Added

--- mqtt_subscriber.py
+++ mqtt_subscriber.py
@@ -2,29 +2,35 @@ import paho.mqtt.client as mqtt
 import save_data as sd
 import json
 from datetime import datetime
-import time
+import time,re
 
+pattern = re.compile("sensor:(?P<sensor>.*),attr:(?P<attr>.*),unit:(?P<unit>.*),value:(?P<value>.*)")
 def on_message(client, userdata, message):
     print("message received " ,str(message.payload.decode("utf-8")))
-    print("message typr " ,type(message.payload))
-    j = json.loads(str(message.payload.decode("utf-8")))
-    ret = sd.save_data({"sensor_id":j['sensor'],
-               "readin_dt":datetime.now().strftime("%Y-%m-%d"),
-               "readin_tm":int(time.time()),
-               "sensor_data":f'{"attribute":{j["sensing"]},"value":{j["value"]},"unit":{j["unit"]}}'
-               })
-    if ret['success']:
-        print("Data Inserted")
-    else:
-        print (ret['message'])
-        raise ValueError()
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
+    msg_str = str(message.payload.decode("utf-8"))
+    regex_srch = pattern.search(msg_str)
+    sensor_id = regex_srch['sensor']
+    unit = regex_srch['unit']
+    attr = regex_srch['attr']
+    value = regex_srch['value']
+    print (f"The value is {sensor_id}")
+    try:
+        ret = sd.persist_data({"sensor_id":sensor_id,
+                   "readin_dt":datetime.now().strftime("%Y-%m-%d"),
+                   "readin_tm":int(time.time()),
+                   "sensor_data":f'{{"attribute":"{attr}","value":{value},"unit":"{unit}"}}'
+                   })
+        if ret['success']:
+            print("Data Inserted")
+        else:
+            print (ret['message'])
+            raise ValueError()
+    except Exception as ex:
+        print (f"The error is {ex}");
     
 client = mqtt.Client("P1")
 client.connect("127.0.0.1")
 
 client.on_message=on_message
-client.subscribe("sensors")
+client.subscribe("sensor")
 client.loop_forever()
--- save_data.py
+++ save_data.py
@@ -1,9 +1,10 @@
-import sqlite3
+import sqlite3,time
 
 DATABASE_ADDRESS = 'sensors.db'
 
 queries = {
-    'insert_reading':"INSERT INTO sensor_readings(sensor_id,readin_dt,readin_tm,sensor_data) VALUES (?,?,?,?)"    
+    'insert_reading':"INSERT INTO sensor_readings(sensor_id,readin_dt,readin_tm,sensor_data) VALUES (?,?,?,?)",
+    'get_reading':"SELECT sensor_id,readin_tm,sensor_data FROM sensor_readings where readin_tm >= ?"
 }
 
 conn = sqlite3.connect(DATABASE_ADDRESS)
@@ -27,19 +28,43 @@ def insert_data(conn,c,query):
         return {'success': True, 'message': None}
     except Exception as ex:
         return {'success': False, 'message': ex}
-        
-def save_data(data):
+    
+
+def select_data(conn,c,query):
+    data = []
+    try:
+        c.execute(*query)
+        records = c.fetchall()
+        for record in records:
+            data.append([item for item in record])
+        return {'data':data,'success': True, 'message': None}
+    except Exception as ex:
+        return {'data':data,'success': False, 'message': ex}
+
+
+def get_data():
+    current_time = int(time.time())
+    query_time = current_time - 60*60
+    query = (queries['get_reading'],(query_time,))
     conn = conn_db()
     c = create_cursor(conn)
+    result = select_data(conn,c,query)
+    close_conn(conn)
+    return result
+    
+def persist_data(data):
+    conn = conn_db()
+    c = create_cursor(conn)
+    print (f"Save_Date: {data}")
     query = (queries['insert_reading'],(data['sensor_id'],data['readin_dt'],data['readin_tm'],data['sensor_data'],),)
     result = insert_data(conn,c,query)
-    
+    close_conn(conn)
     return result
 
 if __name__ == '__main__':
     import time
     from datetime import datetime
-    ret = save_data({"sensor_id":"pot1",
+    ret = persist_data({"sensor_id":"pot1",
                "readin_dt":datetime.now().strftime("%Y-%m-%d"),
                "readin_tm":int(time.time()),
                "sensor_data":'{"attribute":"temperature","value":"25","unit":"c"}'
